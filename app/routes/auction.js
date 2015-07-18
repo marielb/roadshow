7d33b2334de2d20042e18978179be36a65cf6a3a
Ian Suvak <ian.suvak@gmail.com>
2015-07-18 12:30:37 -0400

    save working

--- app/routes/auction.js
+++ app/routes/auction.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var auctionModel = require('../models/auction.js');
+//var auctionModel = require('../models/auction.js');
+var couch = require('../couch.js');
+var uuid = require('node-uuid');
 
 /* Open a page to create a new auction */
 router.get('/', function(req, res, next) {
@@ -18,13 +20,18 @@ router.get('/id/:id', function(req, res, next) {
 /* Send the data to create a new auction*/
 router.post('/', function(req, res, next) {
 	// Image upload to /public/images
+  var auctionModel = {};
+  auctionModel._id = uuid.v4();
   auctionModel.auction_name = req.body.auction_name;
   auctionModel.end_time = req.body.end_time;
   auctionModel.start_bid = req.body.start_bid;
   auctionModel.step = req.body.step;
   auctionModel.image_path = req.files.item_photo.name;
-  auctionModel.save();
-  console.log(req.files);
+  couch.save('auction', auctionModel, function(err, data) {
+    console.log(err);
+    console.log(data);
+  });
+  console.log(auctionModel);
   fs.readFile(req.files.item_photo.path, function (err, data) {
 	  var newPath = __dirname + "/images/";
 	  fs.writeFile(newPath, data, function (err) {
--- app/routes/index.js
+++ app/routes/index.js
@@ -6,8 +6,6 @@ var couch = require('../couch.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   couch.all('auction', {}, function(err, data) {
-    console.log(data);
-    console.log(data.rows);
     res.render('index', {auctions: data.rows});
   })
 });
