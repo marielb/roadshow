eaa9fa7e2ce840c7e52b502413cc42cd8149e5e0
marielb <marielbartolome@gmail.com>
2015-07-19 16:05:01 -0400

    refactor auction model

--- app/models/auction.js
+++ app/models/auction.js
@@ -1,7 +1,61 @@
 var couch = require('../couch.js');
 var userModel = require('../models/user.js');
+var uuid = require('node-uuid');
+var fs = require('fs');
+var path = require("path");
+var schedule = require('node-schedule');
 
 var auctionModel = {
+
+  create: function(prop) {
+    this._id = uuid.v4();
+    this.auctioneer_id = prop.auctioneer_id;
+    this.auction_name = prop.auction_name;
+    this.end_date = this.calculateEndDate(prop.end_date);
+    this.start_bid = prop.start_bid;
+    this.step = prop.step;
+    this.image_path = prop.image_path;
+    this.bid_count = 0;
+  },
+
+  toJSON: function() {
+    return {
+      _id: this._id,
+      auctioneer_id: this.auctioneer_id,
+      auction_name: this.auction_name,
+      end_date: this.end_date.toString(),
+      start_bid: this.start_bid,
+      step: this.step,
+      image_path: this.image_path,
+      bid_count: this.bid_count
+    };
+  },
+
+  save: function(callback) {
+    var self = this;
+    couch.save('auction', this.toJSON(), function(err, data) {
+      if (!err) {
+        schedule.scheduleJob(self.end_date, function(model_id) {
+          couch.id('auction', model_id, function(err, data) {
+            data.closed = true;
+            console.log('Triggered!');
+            couch.save('auction', data, function(err) {
+              if (err) {
+                return false;
+                console.log('Auction expired but failed to close. We done messed up');
+              } else {
+                console.log('GREAT SUCCESS!!');
+              }
+            });
+          });
+        }.bind(null, data._id));
+      } else {
+        return false
+      }
+      callback();
+    });
+  },
+
   validateBid: function(auction_id, user_id, callback) {
     // Fetch the record from the DB before updating it
     couch.id('auction', auction_id, function(err, data) {
@@ -33,6 +87,15 @@ var auctionModel = {
       }
       callback(null, auction_data);
     });
+  },
+
+  // helper date function
+  // it expects time in 24 hour format "HH:MM"
+  calculateEndDate: function (endTimeString) {
+    var hour = parseInt(endTimeString.slice(0,2));
+    var minute = parseInt(endTimeString.slice(3,5));
+    var x = new Date();
+    return new Date(x.getFullYear(), x.getMonth(), (((x.getHours()<=hour&&x.getMinutes()<minute)||x.getHours()<hour)?x.getDate():x.getDate()+1), hour, minute);
   }
 };
 
--- app/routes/auction.js
+++ app/routes/auction.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
 //var auctionModel = require('../models/auction.js');
 var couch = require('../couch.js');
-var uuid = require('node-uuid');
 var userModel = require('../models/user.js');
-var schedule = require('node-schedule');
 var auctionModel = require('../models/auction.js');
 
 /* Open a page to create a new auction */
@@ -48,7 +45,9 @@ router.post('/:id', function(req, res, next) {
         res.cookie('user_id', userModel._id);
         auctionModel.saveBid(data, req.body.rev, userModel._id,
           function(err, data) {
+            console.log(err);
             if (err) {
+              console.log(err.stack)
               res.render('error', {message: err});
             } else {
               data.winning = true;
@@ -66,50 +65,13 @@ router.post('/', function(req, res, next) {
   userModel.login(req.cookies.user_id);
   res.cookie('user_id', userModel._id);
 
-  var auctionModel = {};
-  var endTime = calculateEndDate(req.body.end_date);
-  console.log(endTime);
-  auctionModel._id = uuid.v4();
-  auctionModel.auctioneer_id = userModel._id;
-  auctionModel.auction_name = req.body.auction_name;
-  auctionModel.end_date = endTime;
-  auctionModel.start_bid = req.body.start_bid;
-  auctionModel.step = req.body.step;
-  auctionModel.image_path = req.files.item_photo.name;
-  auctionModel.bid_count = 0;
-  couch.save('auction', auctionModel, function(err, data) {
-    if (!err) {
-      schedule.scheduleJob(endTime, function(model_id) {
-        couch.id('auction', model_id, function(err, data) {
-          data.closed = true;
-          console.log('Triggered!');
-          couch.save('auction', data, function(err) {
-            if (err) {
-              console.log('Auction expired but failed to close. We done messed up');
-            } else {
-              console.log('GREAT SUCCESS!!');
-            }
-          });
-        });
-      }.bind(null, data._id));
-    }
-    console.log(data);
-  });
-  fs.readFile(req.files.item_photo.path, function (err, data) {
-    var newPath = __dirname + "/images/";
-    fs.writeFile(newPath, data, function (err) {
-      res.redirect('/');
-    });
+  var auctionData = req.body;
+  auctionData.auctioneer_id = userModel._id;
+  auctionData.image_path = req.files.item_photo.name;
+  auctionModel.create(auctionData);
+  auctionModel.save(function() {
+    res.redirect('/');
   });
 });
-
-// helper date function
-// it expects time in 24 hour format "HH:MM"
-function calculateEndDate(endTimeString) {
-  var hour = parseInt(endTimeString.slice(0,2));
-  var minute = parseInt(endTimeString.slice(3,5));
-  var x = new Date();
-  return new Date(x.getFullYear(), x.getMonth(), (((x.getHours()<=hour&&x.getMinutes()<minute)||x.getHours()<hour)?x.getDate():x.getDate()+1), hour, minute);
-}
 
 module.exports = router;
