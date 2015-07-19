3ad9ebf3bfba035a00b16cf2f129fc4f14dccc5c
Gunrar Barbell <ughhhhGIT@the.com>
2015-07-19 14:47:24 -0400

    only display open auctions on index

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var couch = require('../couch.js');
-
+var _ = require('underscore');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   couch.all('auction', {}, function(err, data) {
-    res.render('index', {auctions: data.rows});
+  	var rows = [];
+  	_.each(data.rows, function(auction) {
+  		// TODO: if err
+  		console.log(auction.doc);
+  		if (!auction.doc.closed) {
+  			rows.push(auction);
+  		} else {
+  			console.log(auction);
+  		}
+  	});
+    res.render('index', {auctions: rows});
   })
 });
 
