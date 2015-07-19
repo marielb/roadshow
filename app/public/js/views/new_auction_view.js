87eba506183f9ca8c88a5c322849a8b5718cad0c
Doichin Denchev <doichin.denchev@gmail.com>
2015-07-19 01:57:49 -0400

    create auction crop image start

--- app/public/js/views/new_auction_view.js
+++ app/public/js/views/new_auction_view.js
@@ -14,12 +14,19 @@ var AppView = View.extend({
 
   postInitialize: function() {
     console.log('NewAuctionView initialized!');
-    console.log(this.model);
   },
 
   handleImageInput: function(e) {
   	this.model.set('image_name', e.target.files[0].name);
   	console.log(this.model);
+
+    $('#js-canvas-holder').removeClass('canvas_holder_hidden');
+    var context = document.getElementById('js-canvas').getContext('2d');
+    var image = new Image;
+    image.src = URL.createObjectURL(e.target.files[0]);
+    image.onload = function() {
+      context.drawImage(image, 20,20);
+    }
   }
 }, {
   debugName: 'NewAuctionView'
