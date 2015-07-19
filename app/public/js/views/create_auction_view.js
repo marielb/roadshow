ee8b8d711c54b2b9278775c834fbcaf067cd5fe0
Doichin Denchev <doichin.denchev@gmail.com>
2015-07-19 00:08:40 -0400

    more tungsten

--- app/public/js/views/create_auction_view.js
+++ app/public/js/views/create_auction_view.js
@@ -1,15 +1,29 @@
-var CreateAuctionView = Backbone.View.extend({
+/**
+* Create auction view
+*/
+'use strict';
 
-	template: null,
+var View = require('tungstenjs/adaptors/backbone').View;
+var AuctionModel = require('../models/auction_model.js');
+var _ = require('underscore');
 
-	events: {
-	},
+var AppView = View.extend({
+  events: {
+  	'change .js-item-photo-input': 'handleImageInput'
+  },
 
-	initialize: function(options) {
-		_.bindAll(this, 'render');
-		this.render();
-	},
+  model: null,
 
-	render: function() {
-	}
+  handleImageInput: function(e) {
+  	console.log(e.target.files[0].name);
+  	this.model = new AuctionModel({
+  		image_name: e.target.files[0].name
+  	});
+  	this.model.set('image_name', e.target.files[0].name);
+  	console.log(this.model);
+  }
+}, {
+  debugName: 'CreateAuctionView'
 });
+
+module.exports = AppView;
