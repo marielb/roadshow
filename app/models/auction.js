048332b15cbb3f501ec9a88fece3e4ffaeee950f
Ian Suvak <ian.suvak@gmail.com>
2015-07-20 01:09:44 -0400

    fix winning

--- app/models/auction.js
+++ app/models/auction.js
@@ -64,6 +64,8 @@ var auctionModel = {
         parseInt(auction_data.current_bid) + parseInt(auction_data.step) :
         auction_data.start_bid;
     auction_data.bid_count += 1;
+    // TODO: remove this property from ever being set in the auction model, it should be in app level model for auction details (once extant)
+    auction_data.winning = undefined;
     couch.save('auction', auction_data, function(err, doc) {
       if (err) {
         console.log('WAAHHH');
