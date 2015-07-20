94e46d286653e47617d76040001afca1f3406244
marielb <marielbartolome@gmail.com>
2015-07-20 01:59:52 -0400

    redirect to index if search term empty

--- app/routes/search.js
+++ app/routes/search.js
@@ -8,16 +8,20 @@ var uuid = require('node-uuid');
 
 /* Open a page to create a new search */
 router.get('/', function(req, res, next) {
-  var matched_auctions = req.app.fuzzy_auctions.get(req.query.search_term);
-  couch.all('auction', {}, function(err, data) {
-    // TODO: if err
-    var auctions_to_render = _.filter(data.rows, function(auction) {
-      return _.find(matched_auctions, function(ma) {
-        return auction.doc.auction_name == ma[1];
+  if (req.query.search_term) {
+    var matched_auctions = req.app.fuzzy_auctions.get(req.query.search_term);
+    couch.all('auction', {}, function(err, data) {
+      // TODO: if err
+      var auctions_to_render = _.filter(data.rows, function(auction) {
+        return _.find(matched_auctions, function(ma) {
+          return auction.doc.auction_name == ma[1];
+        });
       });
-    });
-    res.render('search', {auctions: auctions_to_render, search_term: req.query.search_term});
-  })
+      res.render('search', {auctions: auctions_to_render, search_term: req.query.search_term});
+    })
+  } else {
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
